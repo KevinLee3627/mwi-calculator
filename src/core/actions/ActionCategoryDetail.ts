5ad14043d8cc34e295886a1774c591dfbce0940e
Kevin Lee <kevinlee3627@gmail.com>
2023-09-01 09:21:58 -0500

    chore: Add ActionCategoryDetail interface

---
+++ src/core/actions/ActionCategoryDetail.ts
@@ -0,0 +1,7 @@
+import { ActionCategoryHrid } from 'src/core/hrid/ActionCategoryHrid';
+
+export interface ActionCategoryDetail {
+  hrid: ActionCategoryHrid;
+  name: string;
+  sortIndex: number;
+}
