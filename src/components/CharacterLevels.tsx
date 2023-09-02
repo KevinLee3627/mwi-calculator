92dc72d173a530f1b24ab0814fe67d6cedcc9950
Kevin Lee <kevinlee3627@gmail.com>
2023-09-01 22:51:39 -0500

    feature: Limit character level inputs to be between 1 and 200

--- src/components/CharacterLevels.tsx
+++ src/components/CharacterLevels.tsx
@@ -2,7 +2,8 @@ import { clientData } from 'src/core/clientData';
 import { SkillHrid } from 'src/core/hrid/SkillHrid';
 import {
   characterLevelSlice,
-  selectCharacterLevel
+  selectCharacterLevel,
+  setLevel
 } from 'src/features/character/characterLevelSlice';
 import { useAppDispatch } from 'src/hooks/useAppDispatch';
 import { useAppSelector } from 'src/hooks/useAppSelector';
@@ -24,15 +25,14 @@ export function CharacterLevels() {
             id={`${skillHrid}_input`}
             name={`${skillHrid}_input`}
             className="input-primary input"
-            defaultValue={levels[skillHrid]}
+            value={levels[skillHrid]}
             onChange={(e) => {
-              // TODO: Debounce?
-              dispatch(
-                characterLevelSlice.actions.setLevel({
-                  skillHrid,
-                  value: parseInt(e.target.value, 10)
-                })
-              );
+              const value = parseInt(e.target.value, 10);
+              if (typeof value !== 'number' || isNaN(value) || value < 1) {
+                dispatch(setLevel({ skillHrid, value: 1 }));
+              } else if (value > 200) {
+                dispatch(setLevel({ skillHrid, value: 200 }));
+              } else dispatch(setLevel({ skillHrid, value }));
             }}
           />
         </td>
