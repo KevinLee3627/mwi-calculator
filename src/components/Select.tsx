02a9502cb4b687d0634822f5fdbaae4057ae2058
Kevin Lee <kevinlee3627@gmail.com>
2023-09-02 12:08:26 -0500

    feature: Creaed Select component

---
+++ src/components/Select.tsx
@@ -0,0 +1,27 @@
+import ReactSelect, { GroupBase, Props, Theme, ThemeConfig } from 'react-select';
+
+export const customColors: Partial<Theme['colors']> = {
+  primary: 'hsl(var(--p))', //
+  primary25: 'hsl(var(--nf))', // option hover
+  neutral0: 'hsl(var(--b1))', // input bg
+  neutral10: 'hsl(var(--p))', // multi option bg
+  neutral20: 'hsl(var(--p))', // input border, divider, chevron
+  neutral30: 'hsl(var(--p))', // input border hover
+  neutral80: 'hsl(var(--nc))' // select text
+};
+
+export const customTheme: ThemeConfig = (theme: Theme) => ({
+  ...theme,
+  colors: {
+    ...theme.colors,
+    ...customColors
+  }
+});
+
+export function Select<
+  Option,
+  IsMulti extends boolean = false,
+  Group extends GroupBase<Option> = GroupBase<Option>
+>(props: Props<Option, IsMulti, Group>) {
+  return <ReactSelect {...props} theme={customTheme} />;
+}
