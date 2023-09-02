10a1ff74b15dbd8323be03831c435ad51e62369a
Kevin Lee <kevinlee3627@gmail.com>
2023-09-02 12:14:15 -0500

    chore: fix imports

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
-import { CharacterEquipment } from 'src/components/CharacterEquipment';
-import { CharacterLevels } from 'src/components/CharacterLevels';
 import { Header } from 'src/components/Header';
+import { CharacterEquipment } from 'src/features/character/equipment/CharacterEquipment';
+import { CharacterLevels } from 'src/features/character/levels/CharacterLevels';
 
 const App = () => {
   return (
