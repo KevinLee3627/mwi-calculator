899ec32e2601522c74b63df23fe98cc99ab15fc4
Kevin Lee <kevinlee3627@gmail.com>
2023-09-02 22:36:07 -0500

    feature: created function to compute stats from player equipment

---
+++ src/features/character/computeStats.ts
@@ -0,0 +1,18 @@
+import { CharacterEquipmentState } from 'src/features/character/equipment/characterEquipmentSlice';
+
+export function computeCharacterStats(equipment: CharacterEquipmentState) {
+  // Look through all player equipment and gather the bonuses up
+  const stats = Object.values(equipment).reduce<Record<string, number>>((acc, val) => {
+    const noncombatStats = val?.equipmentDetail.noncombatStats;
+    if (noncombatStats == null) return acc;
+
+    Object.entries(noncombatStats).forEach((entry) => {
+      const [key, val] = entry;
+      if (acc[key] == null) acc[key] = 0;
+      acc[key] += val;
+    });
+
+    return acc;
+  }, {});
+  return stats;
+}
