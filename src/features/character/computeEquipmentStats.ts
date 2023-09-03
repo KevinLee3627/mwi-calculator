733ee017114097b1c5f6d9ea43b81e12edd00e62
Kevin Lee <kevinlee3627@gmail.com>
2023-09-02 22:37:37 -0500

    refactor: rename computeStats

--- src/features/character/computeEquipmentStats.ts
+++ src/features/character/computeEquipmentStats.ts
@@ -1,6 +1,6 @@
 import { CharacterEquipmentState } from 'src/features/character/equipment/characterEquipmentSlice';
 
-export function computeCharacterStats(equipment: CharacterEquipmentState) {
+export function computeEquipmentStats(equipment: CharacterEquipmentState) {
   // Look through all player equipment and gather the bonuses up
   const stats = Object.values(equipment).reduce<Record<string, number>>((acc, val) => {
     const noncombatStats = val?.equipmentDetail.noncombatStats;
