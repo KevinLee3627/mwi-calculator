ecc0d417ad2c15bc63a3c0b8191f876719ffb862
Kevin Lee <kevinlee3627@gmail.com>
2023-09-03 23:56:41 -0500

    refactor: update svgHrefs to not use /public

--- src/util/svgHrefs.ts
+++ src/util/svgHrefs.ts
@@ -1,8 +1,8 @@
 export const svgHrefs = {
-  buffs: '/public/buffs_sprite.cd54d85e.svg',
-  chat_icons: '/public/chat_icons_sprite.d0df4c1a.svg',
-  items: '/public/items_sprite.30940265.svg',
-  logos: '/public/logos_sprite.834e6b9d.svg',
-  skills: '/public/skills_sprite.3704eb13.svg',
-  upgrades: '/public/upgrades_sprite.289c6337.svg'
+  buffs: '/buffs_sprite.cd54d85e.svg',
+  chat_icons: '/chat_icons_sprite.d0df4c1a.svg',
+  items: '/items_sprite.30940265.svg',
+  logos: '/logos_sprite.834e6b9d.svg',
+  skills: '/skills_sprite.3704eb13.svg',
+  upgrades: '/upgrades_sprite.289c6337.svg'
 };
