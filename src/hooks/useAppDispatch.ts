4b14c352762b786571f82cd4700d4b32050a30aa
Kevin Lee <kevinlee3627@gmail.com>
2023-09-09 14:15:30 -0500

    fix import

--- src/hooks/useAppDispatch.ts
+++ src/hooks/useAppDispatch.ts
@@ -1,4 +1,4 @@
 import { useDispatch } from 'react-redux';
-import { AppDispatch } from 'src/store';
+import { AppDispatch } from 'src/store/store';
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
