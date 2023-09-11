9eea16fac541d7c04ecaafa09bd298986f9db83e
Kevin Lee <kevinlee3627@gmail.com>
2023-09-11 14:23:33 -0500

    refactor: simplified baseTimeToSeconds

--- src/util/baseTimeToSeconds.ts
+++ src/util/baseTimeToSeconds.ts
@@ -1,3 +1,2 @@
 // Minimum action time is 3 seconds
-export const baseTimeToSeconds = (baseTime: number, bonus = 0) =>
-  Math.max((baseTime / 1e9) * (1 / (1 + bonus)), 3);
+export const baseTimeToSeconds = (baseTime: number) => baseTime / 1e9;
