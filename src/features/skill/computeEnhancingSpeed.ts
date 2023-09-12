fc56fec9556c88ff8847c1f20a15b096b53e77b2
Kevin Lee <kevinlee3627@gmail.com>
2023-09-11 21:22:33 -0500

    fix: Fixed computeEnhancingSpeed community buff contribution

--- src/features/skill/computeEnhancingSpeed.ts
+++ src/features/skill/computeEnhancingSpeed.ts
@@ -23,10 +23,13 @@ export function computeEnhancingSpeed({
   const levelBonus = Math.max((enhancingLevel - itemLevel) / 100, 0);
   const equipBonus = equipmentStats['enhancingSpeed'] ?? 0;
 
+  let communityBuffBonus = 0;
   const communityBuffLevel = communityBuffs['/community_buff_types/enhancing_speed'];
-  const { flatBoost, flatBoostLevelBonus } =
-    clientData.communityBuffTypeDetailMap['/community_buff_types/enhancing_speed'].buff;
-  const communityBuffBonus = flatBoost + (communityBuffLevel - 1) * flatBoostLevelBonus;
+  if (communityBuffLevel > 0) {
+    const { flatBoost, flatBoostLevelBonus } =
+      clientData.communityBuffTypeDetailMap['/community_buff_types/enhancing_speed'].buff;
+    communityBuffBonus = flatBoost + (communityBuffLevel - 1) * flatBoostLevelBonus;
+  }
   const bonus = 1 + levelBonus + equipBonus + communityBuffBonus;
   return Math.max(3, baseTime / bonus);
 }
