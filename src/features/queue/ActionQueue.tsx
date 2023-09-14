8d387dfa8adeb04e108c6be7bb957a42e59e0c94
Kevin Lee <kevinlee3627@gmail.com>
2023-09-13 20:51:01 -0500

    Update style on action queue item

--- src/features/queue/ActionQueue.tsx
+++ src/features/queue/ActionQueue.tsx
@@ -1,57 +1,67 @@
 import { EllipsisVerticalIcon } from '@heroicons/react/24/solid';
 import { useMemo } from 'react';
 import { Responsive, WidthProvider } from 'react-grid-layout';
-import { range } from 'src/util/range';
+import { Select } from 'src/components/Select';
+import { clientData } from 'src/core/clientData';
 
 export function ActionQueue() {
   const ResponsiveGridLayout = useMemo(() => WidthProvider(Responsive), []);
-
-  const x = range(0, 10).map((num) => {
-    return (
-      <div key={num} className="m-2 border border-primary">
-        <div className="h-4 w-4 bg-emerald-400"></div>
-        <span>action {num}</span>
-      </div>
-    );
-  });
+  const skillOptions = useMemo(() => {
+    return Object.values(clientData.skillDetailMap).map((detail) => ({
+      label: detail.name,
+      value: detail
+    }));
+  }, []);
 
   const layout = [
     { i: 'a', x: 0, y: 0, w: 1, h: 1 },
     { i: 'b', x: 0, y: 1, w: 1, h: 1 },
     { i: 'c', x: 0, y: 2, w: 1, h: 1 }
   ];
+
+  const elems = layout.map((entry) => {
+    return (
+      <div key={entry.i} className="flex w-24 gap-2">
+        {/* eslint-disable-next-line tailwindcss/no-custom-classname */}
+        <EllipsisVerticalIcon className="queue-drag-handle h-6 w-6" />
+        <Select options={skillOptions} />
+        <Select />
+        <input
+          type="number"
+          className="input-bordered input-primary input"
+          placeholder="# Of Actions"
+        />
+      </div>
+    );
+  });
   return (
-    <dialog id="actionQueueModal" className="modal modal-bottom sm:modal-middle">
-      <form method="dialog" className="modal-box sm:min-w-max">
+    <dialog id="actionQueueModal" className="modal">
+      <div className="modal-box !h-4/6 !max-h-full sm:!w-5/12 sm:!max-w-5xl">
         <h2 className="text-lg font-bold">Action Queue</h2>
         <span>Calculate how long your queue will take.</span>
         <ResponsiveGridLayout
           // eslint-disable-next-line tailwindcss/no-custom-classname
-          className="layout"
+          className="layout z-0"
           style={{
             // https://github.com/react-grid-layout/react-grid-layout/issues/171#issuecomment-336719786
             position: 'relative'
           }}
           layouts={{ default: layout }}
           // lg and xxs are required for some reason? Throws an error if not included.
-          cols={{ default: 1, lg: 1, xxs: 1 }}
-          rowHeight={32}
+          cols={{ default: 1, lg: 1, sm: 1, xs: 1, xxs: 1 }}
+          // TODO: Can we not hardcode this?
+          rowHeight={64}
           draggableHandle=".queue-drag-handle"
+          onDragStop={(layout) => {
+            console.log(layout);
+          }}
+          // https://github.com/react-grid-layout/react-grid-layout/issues/858#issuecomment-428539577
+          // This makes performance ~6x slower than normal, according to documentation
+          useCSSTransforms={false}
         >
-          {/* eslint-disable-next-line prettier/prettier */}
-          <div key="a" className="flex items-center border">
-            {/* eslint-disable-next-line tailwindcss/no-custom-classname */}
-            <EllipsisVerticalIcon className="queue-drag-handle h-8" />
-            <span>a</span>
-          </div>
-          <div key="b" className="border">
-            b
-          </div>
-          <div key="c" className="border">
-            c
-          </div>
+          {elems}
         </ResponsiveGridLayout>
-      </form>
+      </div>
       <form method="dialog" className="modal-backdrop">
         <button>close</button>
       </form>
