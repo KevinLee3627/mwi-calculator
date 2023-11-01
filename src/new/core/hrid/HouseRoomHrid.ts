4fdcf2c0af7a316a220c672bc0d286cc5939d33a
Kevin Lee <kevinlee3627@gmail.com>
2023-10-31 19:04:35 -0500

    feature: add room hrids

---
+++ src/new/core/hrid/HouseRoomHrid.ts
@@ -0,0 +1,17 @@
+export type HouseRoomHrid =
+  | '/house_rooms/archery_range'
+  | '/house_rooms/armory'
+  | '/house_rooms/brewery'
+  | '/house_rooms/dairy_barn'
+  | '/house_rooms/dining_room'
+  | '/house_rooms/dojo'
+  | '/house_rooms/forge'
+  | '/house_rooms/garden'
+  | '/house_rooms/gym'
+  | '/house_rooms/kitchen'
+  | '/house_rooms/laboratory'
+  | '/house_rooms/library'
+  | '/house_rooms/log_shed'
+  | '/house_rooms/mystical_study'
+  | '/house_rooms/sewing_parlor'
+  | '/house_rooms/workshop';
