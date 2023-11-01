d7ce3b7d403e874abcc099bb68a55c3d18613994
Kevin Lee <kevinlee3627@gmail.com>
2023-10-31 19:35:32 -0500

    refactor: setup basic store architecture

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,14 @@ import 'react-data-grid/lib/styles.css';
 
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import App from 'src/new/App';
-// import { store } from 'src/old/store/store';
+import App from 'src/App';
+import { store } from 'src/store/store';
 
 const container = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(container);
 
 root.render(
-  // <Provider store={store}>
-  <App />
-  // </Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
