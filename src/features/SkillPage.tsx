5f2eb2f2af3d8f20f35c3ec0ce919f0d95479bda
Kevin Lee <kevinlee3627@gmail.com>
2023-10-31 22:53:07 -0500

    featuer: add house to skill page top bar

--- src/features/SkillPage.tsx
+++ src/features/SkillPage.tsx
@@ -1,6 +1,7 @@
 import { GameIcon } from 'src/components/GameIcon';
 import { clientData } from 'src/core/clientData';
 import { CommunityBuffTypeHrid } from 'src/core/hrid/CommunityBuffTypeHrid';
+import { HouseRoomHrid } from 'src/core/hrid/HouseRoomHrid';
 import { CharacterLevelInput } from 'src/features/character/levels/CharacterLevelInput';
 import { CharacterEnhancementSelect } from 'src/features/character/loadout/CharacterEnhancementSelect';
 import { CharacterEquipmentSelect } from 'src/features/character/loadout/CharacterEquipmentSelect';
@@ -39,6 +40,7 @@ export function SkillPage() {
         <div className="ml-4">
           <SkillDrinksSelect skillHrid={activeSkillState.activeSkill} />
         </div>
+        <div className="ml-4"></div>
         <div className="ml-4 flex items-end gap-2">
           <CharacterEquipmentSelect
             itemLocationHrid={actionTypeToolLocationMapping[actionTypeHrid]}
@@ -47,6 +49,7 @@ export function SkillPage() {
             itemLocationHrid={actionTypeToolLocationMapping[actionTypeHrid]}
           />
         </div>
+
         <div
           className="form-control ml-4 hover:cursor-pointer"
           onClick={() => openModal('communityBuffsModal')}
@@ -66,6 +69,33 @@ export function SkillPage() {
                     iconName={buffDetail.buff.typeHrid.split('/').at(-1) ?? ''}
                   />
                   <p>{buffLevel}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+
+        <div
+          className="form-control ml-4 hover:cursor-pointer"
+          onClick={() => openModal('houseModal')}
+        >
+          <label className="label">
+            <span className="label-text">House</span>
+          </label>
+          <div className="flex h-12 rounded border border-primary">
+            {Object.entries(house).map((entry) => {
+              const roomHrid = entry[0] as HouseRoomHrid;
+              const roomLevel = entry[1];
+              const roomDetail = clientData.houseRoomDetailMap[roomHrid];
+              if (roomDetail.skillHrid !== activeSkillState.activeSkill) return null;
+
+              return (
+                <div key={roomDetail.hrid} className="grid h-12 w-12 place-items-center">
+                  <GameIcon
+                    svgSetName="skills"
+                    iconName={roomDetail.skillHrid.split('/').at(-1) ?? ''}
+                  />
+                  <p>{roomLevel}</p>
                 </div>
               );
             })}
