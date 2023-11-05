6b2feb673005e9e4c0b859fb4d2a303aa8743799
Kevin Lee <kevinlee3627@gmail.com>
2023-11-05 17:35:57 -0600

    feature: add column visibility dropdown

--- src/features/SkillTable.tsx
+++ src/features/SkillTable.tsx
@@ -5,6 +5,7 @@ import {
   getCoreRowModel,
   getSortedRowModel,
   SortingState,
+  Table,
   useReactTable
 } from '@tanstack/react-table';
 import { useState } from 'react';
@@ -29,6 +30,7 @@ export function SkillTable({ data, actionTypeHrid }: SkillTableProps) {
   const [sorting, setSorting] = useState<SortingState>([
     { id: 'levelRequirement', desc: false }
   ]);
+  const [columnVisibility, setColumnVisibility] = useState({});
 
   const columnHelper = createColumnHelper<ActionDetail>();
   const columns = [
@@ -63,64 +65,115 @@ export function SkillTable({ data, actionTypeHrid }: SkillTableProps) {
   const table = useReactTable({
     data,
     columns,
-    state: { sorting },
+    state: { sorting, columnVisibility },
     onSortingChange: setSorting,
+    onColumnVisibilityChange: setColumnVisibility,
     getCoreRowModel: getCoreRowModel(),
     getSortedRowModel: getSortedRowModel()
   });
 
   return (
-    <table className="table-zebra table">
-      <thead>
-        {table.getHeaderGroups().map((headerGroup) => (
-          <tr key={headerGroup.id}>
-            {headerGroup.headers.map((header) => (
-              <th key={header.id} colSpan={header.colSpan}>
-                {header.isPlaceholder ? null : (
-                  <div
-                    {...{
-                      className: header.column.getCanSort()
-                        ? 'flex cursor-pointer select-none'
-                        : '',
-                      onClick: header.column.getToggleSortingHandler()
-                    }}
-                  >
-                    {flexRender(header.column.columnDef.header, header.getContext())}
-                    {{
-                      asc: <ChevronUpIcon className="h-4 w-4" />,
-                      desc: <ChevronDownIcon className="h-4 w-4" />
-                    }[header.column.getIsSorted() as string] ?? null}
-                  </div>
-                )}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody>
-        {table.getRowModel().rows.map((row) => (
-          <tr key={row.id}>
-            {row.getVisibleCells().map((cell) => (
-              <td key={cell.id}>
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-      <tfoot>
-        {table.getFooterGroups().map((footerGroup) => (
-          <tr key={footerGroup.id}>
-            {footerGroup.headers.map((header) => (
-              <th key={header.id}>
-                {header.isPlaceholder
-                  ? null
-                  : flexRender(header.column.columnDef.footer, header.getContext())}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </tfoot>
-    </table>
+    <div>
+      <ColumnVisibilityDropdown table={table} />
+      <table className="table-zebra table">
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id} colSpan={header.colSpan}>
+                  {header.isPlaceholder ? null : (
+                    <div
+                      {...{
+                        className: header.column.getCanSort()
+                          ? 'flex cursor-pointer select-none'
+                          : '',
+                        onClick: header.column.getToggleSortingHandler()
+                      }}
+                    >
+                      {flexRender(header.column.columnDef.header, header.getContext())}
+                      {{
+                        asc: <ChevronUpIcon className="h-4 w-4" />,
+                        desc: <ChevronDownIcon className="h-4 w-4" />
+                      }[header.column.getIsSorted() as string] ?? null}
+                    </div>
+                  )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          {table.getFooterGroups().map((footerGroup) => (
+            <tr key={footerGroup.id}>
+              {footerGroup.headers.map((header) => (
+                <th key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(header.column.columnDef.footer, header.getContext())}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
+      </table>
+    </div>
+  );
+}
+
+interface ColumnVisibilityDropdownProps {
+  table: Table<ActionDetail>;
+}
+function ColumnVisibilityDropdown({ table }: ColumnVisibilityDropdownProps) {
+  const columnVisibilityCheckboxes = table.getAllLeafColumns().map((col) => {
+    const header = col.columnDef.header ?? '';
+    return (
+      <li key={col.id}>
+        <label>
+          <input
+            type="checkbox"
+            className="checkbox"
+            checked={col.getIsVisible()}
+            onChange={col.getToggleVisibilityHandler()}
+          />
+          {header.toString()}
+        </label>
+      </li>
+    );
+  });
+
+  return (
+    <div className="dropdown">
+      <label tabIndex={0} className="btn-primary btn-outline btn mt-2">
+        Column Select
+      </label>
+      <ul
+        tabIndex={0}
+        className="dropdown-content menu rounded-box z-50 bg-base-200 p-2 shadow"
+      >
+        <li className="mb-2">
+          <label>
+            <input
+              type="checkbox"
+              className="checkbox"
+              checked={table.getIsAllColumnsVisible()}
+              onChange={table.getToggleAllColumnsVisibilityHandler()}
+            />
+            Toggle All
+          </label>
+        </li>
+        {columnVisibilityCheckboxes}
+      </ul>
+    </div>
   );
 }
