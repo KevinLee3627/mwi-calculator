5acaa0b730e80a4f6e0e544d9cc668a978966bd8
Kevin Lee <kevinlee3627@gmail.com>
2023-11-24 23:54:43 -0600

    feature: add missing dependency to useMemo

--- src/features/house/TotalCostTable.tsx
+++ src/features/house/TotalCostTable.tsx
@@ -64,7 +64,7 @@ export function TotalCostTable({
           />
         );
       });
-  }, [startLevel, endLevel, priceOverrides, room.hrid, market]);
+  }, [startLevel, endLevel, priceOverrides, room.hrid, market, setPriceOverrides]);
 
   return (
     <table className="table-zebra table">
