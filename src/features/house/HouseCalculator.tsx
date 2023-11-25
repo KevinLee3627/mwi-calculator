b13be1f2e5c14c4666b2dcada3fb71bc554db0f7
Kevin Lee <kevinlee3627@gmail.com>
2023-11-24 22:24:47 -0600

    feature: add missing dependency to useEffect

--- src/features/house/HouseCalculator.tsx
+++ src/features/house/HouseCalculator.tsx
@@ -22,7 +22,7 @@ export function HouseCalculator() {
 
   useEffect(() => {
     setStartLevel(house[selectedRoom.hrid]);
-  }, [selectedRoom]);
+  }, [selectedRoom, house]);
 
   return (
     <div>
